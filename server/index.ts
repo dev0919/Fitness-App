0e2af69681e780ac2878232d966a1999f10bb0dc
devansh <106925511+dev0919@users.noreply.github.com>
2025-04-07 17:55:44 +0530

    changed ports

--- server/index.ts
+++ server/index.ts
@@ -61,7 +61,7 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   
-  const port = process.env.PORT || 5173;
+  const port = process.env.PORT || 3000;
 
 server.listen(
   {
